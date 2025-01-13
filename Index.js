631136dbb65a89c47514712f454e653297cb52a6
RSanjay <46698559+sammy00@users.noreply.github.com>
2025-01-12 19:05:43 -0800

    New design

--- Index.js
+++ Index.js
@@ -1,29 +1,36 @@
-let hours =document.getElementById('hour');
-let minute =document.getElementById('minutes');
+let hours = document.getElementById('hour');
+let minute = document.getElementById('minutes');
 let seconds = document.getElementById('seconds');
-let ampm = document.getElementById('ampm');
+let AMPM = document.getElementById('AMPM');
+
+let hr = document.querySelector("#hr");
+let mn = document.querySelector("mn");
+let sc = document.querySelector("sc");
+
+setInterval(() =>{
+let h = new Date().getHours();
+let m = new Date().getMinutes();
+let s = new Date().getSeconds();
+
+var am = h >= 12 ? 'PM' : 'AM';
+
+//convert 24hrs clock to 12hours clock
+
+if (h > 12){
+    h = h -12;
+}
+
+//add zero before single digit number
+h = (h < 10) ? "0" + h : h
+m = (m < 10) ? "0" + m : m
+s = (s < 10) ? "0" + s : s
+
+hours.innerHTML = h;
+minute.innerHTML =m;
+seconds.innerHTML=s;
+AMPM.innerHTML=am;
+hr.style.transform = `rotateZ(${m * 30}deg)`;
 
 
-setInterval(()=>{
 
-    let h = new Date().getHours();
-    let m = new Date().getMinutes();
-    let s = new Date().getSeconds();
-
-    var am = h >12 ? 'PM' : 'AM';
-
-    //convert 24hrs clock tpo 12hours clock
-    if (h > 12){
-        h - h -12;
-    }
-    //add zero before single digit number
-
-    h = (h < 10) ? "0" +  h : h
-    m = (m < 10) ? "0" +  m : m
-    s = (s < 10) ? "0" +  s : s
-
-    hours.innerHTML = h;
-    minute.innerHTML = m;
-    seconds.innerHTML = s;
-    ampm.innerHTML = am;
 })
